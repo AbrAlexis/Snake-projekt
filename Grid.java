73f5647db38a02a3cbd5750375bfdbdadac2c119
laurits105 <119811943+laurits105@users.noreply.github.com>
2024-01-02 14:09:06 +0100

    b

--- Grid.java
+++ Grid.java
@@ -4,5 +4,12 @@ public class Grid {
         for (int i = 0; i <= 10; i++) {
             System.out.print(i + "poop ");
         }
+        String[][] b0n3r = new String[10][10];
+        for (int j = 0; j < b0n3r.length; j++) {
+            System.out.println();
+            for (int k = 0; k < b0n3r[j].length; k++) {
+                System.out.print("b0n3r ");
+            }
+        }
     }
 }
