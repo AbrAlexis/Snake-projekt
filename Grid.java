6177fbf1fb9a6fedccef722d37785c4b3b6301ba
Jaruzan <jskramhoeft@gmail.com>
2024-01-02 16:46:33 +0100

    Added comma

--- Grid.java
+++ Grid.java
@@ -1,6 +1,6 @@
 public class Grid {
     public static void main(String[] args) {
-        System.out.println("hello snake gamers");
+        System.out.println("hello, snake gamers");
         for (int i = 0; i <= 10; i++) {
             System.out.print(i + "poop ");
         }
