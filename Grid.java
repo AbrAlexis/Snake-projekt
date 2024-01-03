7044d35b595e12e50338ae77d446e22e3bf3db08
Jaruzan <jskramhoeft@gmail.com>
2024-01-03 09:43:45 +0100

    Snake Class 0.2 + Grid Class 0.2

--- Grid.java
+++ Grid.java
@@ -1,4 +1,12 @@
 public class Grid {
     private int GridX;
     private int GridY;
+
+    public int getGridX() {
+        return GridX;
+    }
+
+    public int getGridY() {
+        return GridY;
+    }
 }
--- Snake.java
+++ Snake.java
@@ -1,3 +1,14 @@
 public class Snake {
+    private int headX;
+    private int headY;
+    private char direction;
+    private int bodyLength;
+
+    public Snake() {
+        this.headX = getGridX();
+        this.headY = getGridY();
+        this.direction = 'w';
+        this.bodyLength = 1;
+    }
 
 }
