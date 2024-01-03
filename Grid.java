36de2a9c8c75f5f66927a8f36e2823273af7ab75
AbrAlexis <alexalex2620@gmail.com>
2024-01-03 10:55:18 +0100

    Added constructor to Grid class

--- Grid.java
+++ Grid.java
@@ -5,14 +5,13 @@ public class Grid {
     private int gridSizeY;
     private boolean[][] grid;
 
-    public Grid(int gridSizeX, int gridSizeY) {
-
+    public Grid() {
+        this.gridSizeX = createGrid("Please enter grid width: ");
+        this.gridSizeY = createGrid("Please enter grid height: ");
         this.grid = new boolean[gridSizeX][gridSizeY];
-
     }
 
     public int getGridSizeX() {
-
         return gridSizeX;
     }
 
@@ -20,8 +19,31 @@ public class Grid {
         return gridSizeY;
     }
 
-    public int[] createGrid(){
-        return {xSize, ySize};
+    public boolean[][] getgrid() {
+        return grid;
+    }
+
+    public int createGrid(String prompt) { // Gets user input for grid width and grid height
+        int size = 0;
+        Scanner console = new Scanner(System.in);
+        System.out.print(prompt);
+        while (true) {
+            String userInputNumber = console.nextLine();
+            // Try catch for making sure the user only inputs integers
+            try {
+                size = (int) Integer.valueOf(userInputNumber);
+                if (size >= 5 && size <= 100) {
+                    break;
+                } else {
+                    System.out.print("Please enter an integer between 5 and 100: ");
+                    continue;
+                }
+            } catch (Exception e) {
+                System.out.print(" Please enter an integer between 5 and 100: ");
+                continue;
+            }
+        }
+        return size;
     }
 
 }
