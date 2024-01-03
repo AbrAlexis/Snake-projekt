200bc0c53790e6238a19b4f1c4243412e7bb12b8
Jaruzan <jskramhoeft@gmail.com>
2024-01-03 10:31:18 +0100

    Food Class 0.1

--- Food.java
+++ Food.java
@@ -1,4 +1,11 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Food {
-    private int x;
-    private int y;
+    private int foodX;
+    private int foodY;
+
+    public Food() {
+        this.foodX = ThreadLocalRandom.current().nextInt(0, getGridSizeX() + 1);
+        this.foodY = ThreadLocalRandom.current().nextInt(0, getGridSizeY() + 1);
+    }
 }
