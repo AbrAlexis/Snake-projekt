e3c29392417fb59ec6ff7b8afd58901a0cc8ef12
AbrAlexis <92082385+AbrAlexis@users.noreply.github.com>
2024-01-04 14:52:20 +0100

    Added SnakeBody class, added a lot of getter methods, added showSnake() method in view class.

--- SimpleSnakeView.java
+++ SimpleSnakeView.java
@@ -17,6 +17,7 @@ public class SimpleSnakeView extends Application {
     public void start(Stage primaryStage) {
         Group root = new Group();
         grid = new Grid();
+        Snake snake = new Snake(grid);
         int sceneSizeX = cellSize * grid.getGridSizeX();
         int sceneSizeY = cellSize * grid.getGridSizeY();
         Scene scene = new Scene(root, sceneSizeX, sceneSizeY, Color.WHITE);
@@ -25,7 +26,7 @@ public class SimpleSnakeView extends Application {
         GraphicsContext gc = canvas.getGraphicsContext2D();
         for (int i = 0; i < sceneSizeX; i += cellSize) {
             for (int j = 0; j < sceneSizeY; j += cellSize) {
-                if ((i + j) % 20 == 0) {
+                if ((i + j) % (2 * cellSize) == 0) {
                     gc.setFill(Color.DARKGREEN);
                 } else {
                     gc.setFill(Color.GREEN);
@@ -33,11 +34,21 @@ public class SimpleSnakeView extends Application {
                 gc.fillRect(i, j, cellSize, cellSize);
             }
         }
-
+        showSnake(gc, snake);
         root.getChildren().add(canvas);
         primaryStage.setTitle("JavaFX Grid Example");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public void showSnake(GraphicsContext gc, Snake snake) {
+        gc.setFill(Color.BLUE);
+        gc.fillRect(snake.getHeadX() * cellSize, snake.getHeadY() * cellSize, cellSize, cellSize);
+        for (int i = 0; i < snake.getSize(); i++) {
+            gc.setFill(Color.BLACK);
+            gc.fillRect(snake.getBody().get(i).getXpos() * cellSize, snake.getBody().get(i).getYpos() * cellSize,
+                    cellSize, cellSize);
+        }
     }
 
     public static void main(String[] args) {
---
+++ SnakeBody.java
@@ -0,0 +1,18 @@
+public class SnakeBody {
+    private int xPos;
+    private int yPos;
+
+    public SnakeBody(int x, int y) {
+        this.xPos = x;
+        this.yPos = y;
+    }
+
+    public int getXpos() {
+        return xPos;
+    }
+
+    public int getYpos() {
+        return yPos;
+    }
+
+}
