6bcb7556283baa4fffc6df45a2459bfacde186b9
laurits105 <119811943+laurits105@users.noreply.github.com>
2024-01-04 12:28:49 +0100

    added randomly spawning food to grid

--- SimpleSnakeView.java
+++ SimpleSnakeView.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 public class SimpleSnakeView extends Application {
     private Grid grid;
     private int cellSize = 10;
+    private Food food;
 
     @Override
     public void start(Stage primaryStage) {
@@ -28,6 +29,12 @@ public class SimpleSnakeView extends Application {
         Rectangle head = new Rectangle(500 / grid.getGridSizeX(), 500 / grid.getGridSizeY());
         head.setFill(Color.LIMEGREEN);
         gridpane.add(head, grid.getGridSizeX() / 2, grid.getGridSizeY() / 2);
+
+        // Food
+        food = new Food(grid);
+        Rectangle mad = new Rectangle(500 / grid.getGridSizeX(), 500 / grid.getGridSizeY());
+        mad.setFill(Color.RED);
+        gridpane.add(mad, food.getFoodX(), food.getFoodY());
 
         Scene scene = new Scene(gridpane, sceneSizeX, sceneSizeY);
 
