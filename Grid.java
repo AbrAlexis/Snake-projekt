9f289e88a6ec85750c864f8efde127b0c3fdbc8b
AbrAlexis <92082385+AbrAlexis@users.noreply.github.com>
2024-01-06 15:48:31 +0100

    grid

--- Grid.java
+++ Grid.java
@@ -59,4 +59,13 @@ public class Grid {
         return size;
     }
 
+    public void testDraw() {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid.length; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
 }
